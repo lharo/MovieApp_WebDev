b0900f3d618e9cb2a38ad353fec8555ebc6ab194
Dante Spravkin Galina <0239087@up.edu.mx>
2022-10-24 18:46:36 -0500

    TareaDante

--- src/hooks/useFetchTrending.js
+++ src/hooks/useFetchTrending.js
@@ -1,18 +1,19 @@
 import { useEffect, useState } from "react"
 import { getTrending } from "../helpers/getTrending";
 
-export const useFetchTrending = () => {
+export const useFetchTrending = ( {media, time}) => {
+
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const getTrendingMovies = async (media_type, time_window) => {
-    const results = await getTrending(media_type, time_window);
+    const results = await getTrending(media_type= media, time_window= time);
     setMovies(results);
     setIsLoading(false);
   }
 
   useEffect(() => {
-    getTrendingMovies();
+    getTrendingMovies( getTrendingMovies(media, time) )
   }, []);
   
   return {
--- src/pages/Trending.js
+++ src/pages/Trending.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { MovieCard } from '../components/MovieCard';
 import { useFetchTrending } from '../hooks/useFetchTrending'
 
 export const Trending = () => {
 
+  const [type, setType] = useState('')
+  const [timeLapse, setTimeLapse] = useState('')
+
   const { movies } = useFetchTrending();
 
   return (
-    <div className='container row row-cols-1 row-cols-md-3 g-3'>
-      { movies.map((movie) => 
-        <MovieCard 
-          key={movie.id}
-          { ...movie }
-        />
-      )}
-    </div>
+    <>
+      <div  className='select media'>
+        <label>Elige el tipo de contenido</label>
+        <select name="media type">
+        <option vale={type} onChange={setType("all")} >all</option>
+        <option vale={type} onChange={setType("movie")} >movie</option>
+        <option vale={type} onChange={setType("tv")} >tv</option>
+        </select>
+      </div>
+
+      <div className='timeLapse'>
+        <labe>Elegir un periodo:</labe>
+        <select>
+          <option value={timeLapse} onChange={ setTimeLapse('day')  } >day</option>
+          <option value={timeLapse} onChange={ setTimeLapse('week')  } >week</option>
+        </select>
+      </div>
+
+      <div className='container row row-cols-1 row-cols-md-3 g-3'>
+        { movies.map((movie) => 
+          <MovieCard 
+            key={movie.id}
+            { ...movie }
+          />
+        )}
+      </div>
+    </>
+  
   )
 }
